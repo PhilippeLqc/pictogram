4f27bd6f2c4c1f0c6accf8cb10dfbfce27c567a9
Le Quoc <philippe.lequoc@gmail.com>
2024-05-25 22:06:03 +0200

    creation profile page

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { useUserContext } from "@/context/AuthContext";
+import { useGetCurrentUser } from "@/lib/react-query/queryAndMutations";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { data: creator, isPending } = useGetCurrentUser();
+  const { user } = useUserContext();
+
+  return (
+    <div className="flex flex-row items-center justify-center gap-4">
+      <img
+        src={creator?.imageUrl}
+        alt="user profile"
+        width={150}
+        height={150}
+        className="rounded-full"
+      />
+      <h1>{user?.name}</h1>
+      {user?.id === creator?.id ? (
+        <Button>Edit Profile</Button>
+      ) : (
+        <Button>Follow</Button>
+      )}
+    </div>
+  );
 };
 
 export default Profile;
